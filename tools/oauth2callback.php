5d8c1ba8d2e98103df7665f9404f3d868677fe53
deconf <nylas@DESKTOP-EUDCGRP>
2024-01-16 20:50:35 +0200

    file cleanup

--- tools/oauth2callback.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-if ( $_GET['state'] && $_GET['code'] ) {
-	$redirect_uri = esc_url_raw( $_GET['state'] . '&seiwp_access_code=' . $_GET['code'] );
-	header( 'Location: ' . filter_var( $redirect_uri, FILTER_SANITIZE_URL ) );
-}
